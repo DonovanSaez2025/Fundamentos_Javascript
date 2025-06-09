342980168c34e129a4fa314630162c9bd97e8d2d
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-06-09 09:19:28 -0400

    Ejemplo strings
    Añadimos una carpeta 04 donde hicimos ejemplos de strings además añadí la carpeta images a todos los statics de las carpetas

---
+++ 04_string_example/static/js/script.js
@@ -0,0 +1,28 @@
+// Ejemplos de string
+
+function usarString() {
+    // Variable tipo texto que permite ingresar cualquier nombre
+    let nombre = prompt("Ingrese su nombre: ");
+    // Asignamos valor a primeraLetra con posición 0 de variable nombre
+    let primeraLetraNombre = nombre[0];
+    // Uso de length --> largo de una variable
+    let ultimaLetraNombre = nombre[nombre.length - 1];
+    // mostrando resultado
+    alert("Primera letra del nombre es: " + primeraLetraNombre +
+        "\nÚltima letra es: " + ultimaLetraNombre
+    );
+}
+
+// Tarea: Imprimir segunda letra y penúltima de el apellido
+function manipularApellido() {
+    // Variableq ue permite ingresar cualquier apellido
+    let apellido = prompt("Ingrese su apellido: ");
+    // Obtenemos la letra que está en la posición 1 en la variable apellido
+    let segundaLetraApellido = apellido[1];
+    // Usando length para obtener la penúltima letra
+    let penultimaLetraApellido = apellido[apellido.length - 2];
+    // mostrando resultado
+    alert("Segunda letra del apellido es: " + segundaLetraApellido +
+        "\nPenúltima letra del apellido es: " + penultimaLetraApellido
+    );
+}
