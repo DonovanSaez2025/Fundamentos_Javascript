9c79198726c7a1492169f2e9fc4343bf9a160715
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-12-01 08:29:59 -0300

    f
    f

---
+++ 09_arreglos_y_matrices/static/js/script.js
@@ -0,0 +1,4 @@
+function fraseArreglo() {
+    let palabras = ["Juan", "corre", "rápidamente", "feliz", "parque"];
+    alert(`${palabras[0]} ${palabras[1]} ${palabras[3]} y ${palabras[2]} por el ${palabras[4]}`);
+}
